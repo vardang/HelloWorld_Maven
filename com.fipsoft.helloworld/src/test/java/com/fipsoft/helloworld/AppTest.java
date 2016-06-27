294551fa8d9a4a5e226a59f9720ff443468fc37e
Vardan <vardang@linux-0o2e.suse>
2016-06-28 01:11:41 +0400

    added

--- com.fipsoft.helloworld/src/test/java/com/fipsoft/helloworld/AppTest.java
+++ com.fipsoft.helloworld/src/test/java/com/fipsoft/helloworld/AppTest.java
@@ -1,8 +1,12 @@
 package com.fipsoft.helloworld;
 
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
@@ -10,29 +14,10 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+  public void test(){
+        
+       Greeting greet = new Greeting();
+        String greetingString = greet.toString();
+       assertEquals("howdy",greetingString);
+  }
 }
